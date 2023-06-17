c2cf86ef6e518623e659ea9a03aa0ab4227b1251
Najmi Alwan <najmialwan07@gmail.com>
2023-06-17 21:31:45 +0700

    Commit Modul 9

--- app/src/main/java/com/example/modul8_retrofitgetapi/ApiService.kt
+++
@@ -1,8 +0,0 @@
-package com.example.modul8_retrofitgetapi
-
-import retrofit2.Call
-import retrofit2.http.GET
-interface ApiService {
-    @GET("datamahasiswa/")
-    fun getdatamahasiswa(): Call<ApiResponse>
-}
--- app/src/main/java/com/example/modul8_retrofitgetapi/MainActivity.kt
+++ app/src/main/java/com/example/modul8_retrofitgetapi/MainActivity.kt
@@ -1,29 +1,40 @@
 package com.example.modul8_retrofitgetapi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import com.example.modul8_retrofitgetapi.Network.ApiClient
 import com.example.modul8_retrofitgetapi.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: RVAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         adapter = RVAdapter(this@MainActivity, arrayListOf())
-        binding.rvMain.adapter = adapter
-        binding.rvMain.setHasFixedSize(true)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.setHasFixedSize(true)
+
+    }
+    override fun onResume() {
+        super.onResume()
         remoteGetdatamahasiswa()
     }
+
     private fun remoteGetdatamahasiswa() {
-        ApiClient.apiService.getdatamahasiswa().enqueue(object :
-            Callback<ApiResponse> {
-            override fun onResponse(call: Call<ApiResponse>,
-                                    response: Response<ApiResponse>) {
+        ApiClient.apiService.remoteGetdatamahasiswa().enqueue(object : Callback<ApiResponse> {
+            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
                 if (response.isSuccessful) {
                     val apiResponse = response.body()
                     val data = apiResponse?.data
@@ -32,8 +43,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
-            override fun onFailure(call: Call<ApiResponse>, t:
-            Throwable) {
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
                 Log.d("Error", t.stackTraceToString())
             }
         })
@@ -41,4 +51,16 @@ class MainActivity : AppCompatActivity() {
     private fun setDataToAdapter(data: List<Mahasiswa>) {
         adapter.setData(data)
     }
+    fun Insert(view: View) {
+        val intent = Intent(this, com.example.modul8_retrofitgetapi.Fitur.Insert::class.java)
+        startActivity(intent)
+    }
+    override fun onBackPressed() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
+    }
+
 }
